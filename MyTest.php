a3b2df4cd83546b2993bd2ccfa98445a24fa2d7e
David <labarta.david@gmail.com>
2015-04-15 21:23:20 +0200

    First commit

---
+++ MyTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 15/04/15
+ * Time: 18:25
+ */
+
+class MyTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     *
+     */
+    public function testSuma()
+    {
+        $this->assertTrue(true);
+
+        for($i=0; $i< 10;$i++)
+            echo($i.'-');
+
+    }
+}
+ 
